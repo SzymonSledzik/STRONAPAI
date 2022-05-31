810162a04d95aa812c9f010e7aec0c775d0192e2
= <szymon.sledzik02@gmail.com>
2022-05-31 23:00:21 +0200

    XDD
    XDDDDDD

--- php/index.php
+++ php/index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="../style/style-other.css">
 	<title>Strona w PHP</title>
 </head>
+
 <body>
 
 	<form method="POST">
@@ -15,46 +17,48 @@
 
 	<?php
 
-		include("connection.php");
+	include("connection.php");
 
-		if(isset($_POST['submit'])) {
-			submit_data();
-		}
+	if (isset($_POST['submit'])) {
+		submit_data();
+	}
 
-		function get_data() {
-			global $con, $data;
+	function get_data()
+	{
+		global $con, $data;
 
-			$char_array = ['K', 'S', 'M', 'I', 'T'];
-			$data = [];
+		$char_array = ['K', 'S', 'M', 'I', 'T'];
+		$data = [];
 
-			for($i=0; $i<sizeof($char_array); $i++) {
-				$basic_sql = "SELECT * FROM test WHERE id_char='{$char_array[$i]}'";
-				$basic_query = mysqli_query($con, $basic_sql);
+		for ($i = 0; $i < sizeof($char_array); $i++) {
+			$basic_sql = "SELECT * FROM test WHERE id_char='{$char_array[$i]}'";
+			$basic_query = mysqli_query($con, $basic_sql);
 
-				$counter = 1;
-				while($row = mysqli_fetch_array($basic_query)) {
-					$data[$char_array[$i]][$counter] = [
-						"Question"=>$row['Pytanie']
-					];
-					$counter++;
-				}
+			$counter = 1;
+			while ($row = mysqli_fetch_array($basic_query)) {
+				$data[$char_array[$i]][$counter] = [
+					"Question" => $row['Pytanie']
+				];
+				$counter++;
 			}
-
-			return $data;
 		}
 
-		function print_data() {
-			$array_data = get_data();
+		return $data;
+	}
 
-			$char_array = ['K', 'S', 'M', 'I', 'T'];
+	function print_data()
+	{
+		$array_data = get_data();
 
-			$count = 1;
-			for($i=1; $i<=2; $i++) {
-				for($z=0; $z<sizeof($char_array); $z++) {
-					//print question
-					$question = $count.'. '.($array_data[$char_array[$z]][$i]["Question"])."<br>";
-					//create questions
-					echo "
+		$char_array = ['K', 'S', 'M', 'I', 'T'];
+
+		$count = 1;
+		for ($i = 1; $i <= 10; $i++) {
+			for ($z = 0; $z < sizeof($char_array); $z++) {
+				//print question
+				$question = $count . '. ' . ($array_data[$char_array[$z]][$i]["Question"]) . "<br>";
+				//create questions
+				echo "
 						<div class='div' id='div$count'>
 							<p>$question</p>
 
@@ -66,53 +70,50 @@
 
 						</div>
 					";
-					$count++;
+				$count++;
+			}
+		}
+	}
+
+	print_data();
+
+
+	function submit_data()
+	{
+		$points = [
+			"K" => 0,
+			"S" => 0,
+			"M" => 0,
+			"I" => 0,
+			"T" => 0
+		];
+
+		$array_with_char = ['K', 'S', 'M', 'I', 'T'];
+
+		$error = 0;
+		for ($i = 1; $i <= 2; $i++) {
+			for ($z = 0; $z < sizeof($array_with_char); $z++) {
+				if (isset($_POST["checkbox" . $i . $array_with_char[$z]])) {
+					if ($_POST["checkbox" . $i . $array_with_char[$z]] == "Tak") {
+						$points[$array_with_char[$z]] += 1;
+					}
+				} else if (!isset($_POST["checkbox" . $i . $array_with_char[$z]])) {
+					$error = 1;
 				}
 			}
 		}
 
-		print_data();
+		if ($error == 1) {
+			echo "<script>alert('Nie zaznaczono odpowiedzi!');</script>";
+		} else {
+			//highest value
+			$maxVal = max($points);
+			//search key fo the highest valueś
+			$maxKey = array_search($maxVal, $points);
 
-
-		function submit_data() {
-			$points = [
-				"K"=> 0,
-				"S"=> 0,
-				"M"=> 0,
-				"I"=> 0,
-				"T"=> 0
-			];
-		
-			$array_with_char = ['K', 'S', 'M', 'I', 'T'];
-
-			$error = 0;
-			for($i=1; $i<=2; $i++) {
-				for($z=0; $z<sizeof($array_with_char); $z++) {
-					if(isset($_POST["checkbox".$i.$array_with_char[$z]])) {
-						if($_POST["checkbox".$i.$array_with_char[$z]] == "Tak") {
-								$points[$array_with_char[$z]] += 1;
-							}
-					}
-					else if(!isset($_POST["checkbox".$i.$array_with_char[$z]])) {
-						$error = 1;
-					}
-
-				}
-			}
-
-			if($error == 1) {
-				echo "<script>alert('Nie zaznaczono odpowiedzi!');</script>";
-			}
-			else {	
-				//highest value
-				$maxVal = max($points);
-				//search key fo the highest valueś
-				$maxKey = array_search($maxVal, $points);
-
-				echo "Highest score for $maxKey: $maxVal";
-			}
-
+			echo "Highest score for $maxKey: $maxVal";
 		}
+	}
 
 
 
@@ -120,18 +121,17 @@
 
 
 	<script type="text/javascript">
-		
 		window.onload = function() {
 			//get all questions div
 			let questions_div = document.querySelectorAll(".div");
 
-			for(let i=0; i<questions_div.length; ++i) {
+			for (let i = 0; i < questions_div.length; ++i) {
 				//append question div into holder
 				document.querySelector("#holder").appendChild(questions_div[i]);
 			}
-		}			
-
+		}
 	</script>
 
 </body>
+
 </html>
