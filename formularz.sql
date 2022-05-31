f4d50aeee85bbbb4aab7c91c48e78650ff776547
= <szymon.sledzik02@gmail.com>
2022-05-31 23:02:04 +0200

    Update formularz.sql

--- formularz.sql
+++ formularz.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 31 Maj 2022, 21:51
--- Wersja serwera: 10.4.21-MariaDB
--- Wersja PHP: 8.0.11
+-- Czas generowania: 31 Maj 2022, 23:01
+-- Wersja serwera: 10.4.24-MariaDB
+-- Wersja PHP: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -47,7 +47,47 @@ INSERT INTO `pytania` (`id`, `id_char`, `Pytanie`) VALUES
 (7, 'K', 'Czy byłeś kiedyś odpowiedzialny za taki projekt albo zadanie, które wymagało dopilnowania różnych szczegółów, aby mogło zostać wykonane w całości? '),
 (8, 'K', 'Czy kiedyś byłeś odpowiedzialny za planowanie działań, które miał wykonać ktoś inny? '),
 (9, 'K', 'Czy wolisz raczej kierować pracą grupy niż być jej członkiem?'),
-(10, 'K', 'Czy myślisz że masz dużą siłę przekonywania innych do swoich racji? ');
+(10, 'K', 'Czy myślisz że masz dużą siłę przekonywania innych do swoich racji? '),
+(25, 'S', 'Czy zdarza Ci się pomóc innym nie oczekując niczego\r\nw zamian?'),
+(26, 'S', 'Czy zdarza Ci się mieć lepsze pomysły od Twoich\r\nkolegów/koleżanek na rozwiązanie jakiegoś zadania?'),
+(27, 'S', 'Czy zdarzało Ci się pomagać ludziom, którzy są czymś\r\nzmartwieni? '),
+(28, 'S', 'Czy chciałbyś podczas wakacji dorabiać sobie pracą\r\nankietera i pytać ludzi o ich opinie w różnych sprawach?'),
+(29, 'S', 'Czy mógłbyś przebywać z ludźmi, którzy są chorzy lub mają\r\njakieś problemy życiowe?'),
+(30, 'S', 'Czy chciałbyś mieć pracę, która jest związana z poprawą\r\nwarunków życia innych ludzi?'),
+(31, 'S', 'Czy chciałbyś mieć pracę związaną ze służbą dla\r\nokreślonego środowiska? '),
+(32, 'S', 'Czy chciałbyś mieć stanowisko, które wymaga kontaktu\r\nz ludźmi przez cały dzień?'),
+(33, 'S', 'Czy współpraca z ludźmi przychodzi ci łatwo?'),
+(34, 'S', 'Czy potrafisz organizować i nadzorować pracę innych ludzi?'),
+(35, 'M', 'Czy sprawia Ci satysfakcję doprowadzenie jakiegoś zadania\r\ndo końca?'),
+(36, 'M', 'Czy wolałbyś mieć taką pracę, w której zawsze będziesz\r\nwiedział, czego się od ciebie oczekuje?'),
+(37, 'M', 'Czy projekty lub inne prace wykonujesz dokładnie, krok po\r\nkroku?'),
+(38, 'M', 'Czy chciałbyś opierać się w swojej pracy na wyraźnie\r\nokreślonych zasadach i regułach?'),
+(39, 'M', 'Czy potrafisz pracować nad jakimś zadaniem przez dłuższy\r\nczas?'),
+(40, 'M', 'Czy lubisz pracować według wskazówek innych ludzi?'),
+(41, 'M', 'Czy chciałbyś mieć taką pracę, gdzie twoje czynności są na\r\nogół ściśle kontrolowane?'),
+(42, 'M', 'Czy podejmujesz się nowego zadania wtedy, gdy skończysz\r\npoprzednie?'),
+(43, 'M', 'Czy chciałbyś mieć stanowisko pracy, które wymaga stałego\r\nposzerzania swoich umiejętności?'),
+(44, 'M', 'Czy umiesz przyjmować polecenia od innych?'),
+(45, 'I', 'Czy czujesz się dobrze, gdy jesteś odpowiedzialny za wiele\r\nspraw jednocześnie?'),
+(46, 'I', 'Czy lubisz kanał naukowy Discovery Chanell, czasopismo\r\nNational Geographic lub inne tego typu?'),
+(47, 'I', 'Czy lubisz zagłębiać się w problemy, nad którymi pracujesz?'),
+(48, 'I', 'Czy rozwiązując problem, chciałbyś opierać się na\r\nniepodważalnych faktach?'),
+(49, 'I', 'Czy podobałaby Ci się praca, w której każdy dzień niósłby\r\nnowe, różnorodne zadania?'),
+(50, 'I', 'Czy chciałbyś wykonywać pracę archeologa?'),
+(51, 'I', 'Czy jesteś w stanie radzić sobie w sytuacji, gdy nieustannie\r\ndzieje się coś nowego i ciekawego?'),
+(52, 'I', 'Czy lubisz realizować zadania, które pozwalają odkrywać\r\nnieznane Ci rzeczy, zagadnienia, fakty?'),
+(53, 'I', 'Czy eksperymentowałeś i wykorzystywałeś różne pomysły,\r\naby rozwiązać jakiś problem?'),
+(54, 'I', 'Czy lubisz takie działania, których wynik daje się obiektywnie\r\nzmierzyć?'),
+(55, 'T', 'Czy lubisz konstruować i naprawiać różne rzeczy?'),
+(56, 'T', 'Czy potrafisz projektować, wymyślać lub tworzyć różne\r\nprzedmioty?'),
+(57, 'T', 'Czy masz jakieś własne hobby?'),
+(58, 'T', 'Czy odwiedzasz w Internecie portale z grami i łamigłówkami\r\nlogicznymi?'),
+(59, 'T', 'Czy podoba Ci się praca wymagająca używania narzędzi\r\nlub maszyn?'),
+(60, 'T', 'Czy chciałbyś pracować z materiałem takim jak drewno, glina,\r\nmetal, kamień, tkanina?'),
+(61, 'T', 'Czy chciałbyś być przez cały dzień operatorem jakiegoś\r\nurządzenia?'),
+(62, 'T', 'Czy potrafisz wyjaśnić zasadę działania telefonu\r\nkomórkowego?'),
+(63, 'T', 'Czy cieszy Cię, kiedy dzięki książkom lub programom\r\ntelewizyjnym możesz dowiedzieć się, jak działają różne\r\nurządzenia?'),
+(64, 'T', 'Czy raczej wolałbyś pracę nie wymagająca kontaktu\r\nz ludźmi?');
 
 -- --------------------------------------------------------
 
@@ -101,7 +141,7 @@ ALTER TABLE `test`
 -- AUTO_INCREMENT dla tabeli `pytania`
 --
 ALTER TABLE `pytania`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
 
 --
 -- AUTO_INCREMENT dla tabeli `test`
