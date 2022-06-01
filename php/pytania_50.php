4d1b982cd53cc9b762250ed425038c664551b510
= <szymon.sledzik02@gmail.com>
2022-06-01 12:10:16 +0200

    Update pytania_50.php

--- php/pytania_50.php
+++ php/pytania_50.php
@@ -5,136 +5,79 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="../style/main.css">
-	<title>Strona w PHP</title>
+	<title>Pytnia_26</title>
 </head>
 
 <body>
-
 	<form method="POST">
 		<div id="holder"></div>
 	</form>
-
 	<?php
 
 	include("connection.php");
 
-	if (isset($_POST['submit'])) {
-		$result = submit_data();
-		echo "<script>alert('$result');</script>";
+	if (isset($_POST['send'])) {
+		count_ponts();
 	}
 
-	function get_data()
+	function main()
 	{
-		global $con, $data;
-
-		$char_array = ['K', 'S', 'M', 'I', 'T'];
+		global $con;
 		$data = [];
-
-		for ($i = 0; $i < sizeof($char_array); $i++) {
-			$basic_sql = "SELECT * FROM pytania WHERE id_char='{$char_array[$i]}'";
-			$basic_query = mysqli_query($con, $basic_sql);
-
-			$counter = 1;
-			while ($row = mysqli_fetch_array($basic_query)) {
-				$data[$char_array[$i]][$counter] = [
-					"Question" => $row['Pytanie']
-				];
-				$counter++;
-			}
+		$sql = "SELECT Pytanie FROM pytania2";
+		$query = mysqli_query($con, $sql);
+		$counter = 1;
+		while ($row = mysqli_fetch_array($query)) {
+			$data[$counter] = [
+				"Question" => $row['Pytanie']
+			];
+			$counter++;
 		}
-
 		return $data;
 	}
-
 	function print_data()
 	{
-		$array_data = get_data();
-
-		$char_array = ['K', 'S', 'M', 'I', 'T'];
-
-		$count = 1;
-		for ($i = 1; $i <= 10; $i++) {
-			for ($z = 0; $z < sizeof($char_array); $z++) {
-				//print question
-				$question = $count . '. ' . ($array_data[$char_array[$z]][$i]["Question"]) . "<br>";
-				//create questions
-				echo "
-						<div class='div' id='div$count'>
-							<p>$question</p>
-
-							<label for='true$i$char_array[$z]' class='label'>Tak</label>
-							<input type='radio' name='checkbox$i$char_array[$z]' id='true$i$char_array[$z]' value='Tak'>
-
-							<label for='nie$i$char_array[$z]' class='label'>Nie</label>
-							<input type='radio' name='checkbox$i$char_array[$z]' id='nie$i$char_array[$z]'>
-
-						</div>
-					";
-				$count++;
-			}
+		$data = main();
+		for ($i = 1; $i <= sizeof($data); $i++) {
+			$question = $data[$i]['Question'];
+			echo "
+					<div class='div'>
+						<p>$i. $question</p>
+						<input name='inputs$i' type='number' max=2 min=0 value=0>
+					</div>
+				";
 		}
-		echo "<div id='holder'> <button class='button' type='submit' value='submit'>Wyślij</button> </div>";
+		echo "<div id='button_holder'> <button class='button' type='submit' name='send' value='submit'>Wyślij</button> </div>";
 	}
 
 	print_data();
 
 
-	function submit_data()
+	function count_ponts()
 	{
-		$points = [
-			"K" => 0,
-			"S" => 0,
-			"M" => 0,
-			"I" => 0,
-			"T" => 0
-		];
+		$suma = 0;
+		$data = main();
 
-		$array_with_char = ['K', 'S', 'M', 'I', 'T'];
-
-		$error = 0;
-		for ($i = 1; $i <= 10; $i++) {
-			for ($z = 0; $z < sizeof($array_with_char); $z++) {
-				if (isset($_POST["checkbox" . $i . $array_with_char[$z]])) {
-					if ($_POST["checkbox" . $i . $array_with_char[$z]] == "Tak") {
-						$points[$array_with_char[$z]] += 1;
-					}
-				} else if (!isset($_POST["checkbox" . $i . $array_with_char[$z]])) {
-					$error = 1;
-				}
+		for ($i = 1; $i <= sizeof($data); $i++) {
+			if (isset($_POST['inputs' . $i])) {
+				$suma += $_POST['inputs' . $i];
 			}
 		}
-
-		if($error == 1) {
-			return "Nie zaznaczono odpowiedzi";
-		}
-		else {
-			//highest value
-			$maxVal = max($points);
-			//search key fo the highest valueś
-			$maxKey = array_search($maxVal, $points);
-
-			$results = "Highest score on $maxKey: $maxVal";
-			return $results;
-		}
+		echo "<script>alert('Wynik: $suma');</script>";
 	}
-
-
 
 	?>
 
-
 	<script type="text/javascript">
 		window.onload = function() {
-			//get all questions div
-			let questions_div = document.querySelectorAll(".div");
+			let div = document.querySelectorAll('.div');
+			let holder = document.querySelector('#holder');
 
-			for (let i = 0; i < questions_div.length; ++i) {
-				//append question div into holder
-				document.querySelector("#holder").appendChild(questions_div[i]);
+			for (let i = 0; i < div.length; ++i) {
+				holder.appendChild(div[i]);
 			}
-			// let button = document.createElement("button");
-			// button.innerHTML = "Wyślij";
-			// document.querySelector("#holder").appendChild(button);
+
+			holder.appendChild(document.querySelector("#button_holder"));
 		}
 	</script>
 
